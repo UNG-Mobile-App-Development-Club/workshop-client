d3cc58cd24dd0c916a4363ca10ee95f6d9f67763
kali <kali@kali>
2025-09-10 14:42:21 -0400

    Followed the workshop and made the API work

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "net/http"
+import "io"
+import "encoding/json"
+
 func main() {
-    println("Zach")
+    res, _ := http.Get("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium")
+
+        triviaRes := TriviaResponse{}
+
+        bytes, _ := io.ReadAll(res.Body)
+
+        json.Unmarshal(bytes, &triviaRes)
+
+        for _, result := range triviaRes.Results {
+            println(result.Category + ": " + result.Question)
+        }
 }
---
+++ messages.go
@@ -0,0 +1,11 @@
+package main
+
+type TriviaResponse struct {
+	ResponseCode int `json:"response_code"`
+	Results      []Result `json:"results"`
+}
+
+type Result struct {
+	Question string `json:"question"`
+	Category string `json:"category"`
+}
